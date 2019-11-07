dc4ecc075fabd44ca56de8a458df70a591ffe9cf
SatoShun <shun.sato1@gmail.com>
2019-11-07 10:46:07 +0000

    first

---
+++ app/src/main/java/com/github/satoshun/example/home/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.github.satoshun.example.home
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.satoshun.example.databinding.HomeFragBinding
+
+class HomeFragment : Fragment() {
+  private lateinit var binding: HomeFragBinding
+
+  override fun onCreateView(
+    inflater: LayoutInflater,
+    container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View? {
+    binding = HomeFragBinding.inflate(inflater, container, false)
+    return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    binding.submit.setOnClickListener {
+    }
+  }
+}
